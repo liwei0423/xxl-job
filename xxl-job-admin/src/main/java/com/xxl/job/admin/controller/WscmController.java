29a4079fbefcca91195dc80a76c7083d1e22e154
fahai <786541343@qq.com>
2025-08-28 16:09:35 +0800

    添加自定义健康检查接口wscm/heart

---
+++ xxl-job-admin/src/main/java/com/xxl/job/admin/controller/WscmController.java
@@ -0,0 +1,24 @@
+package com.xxl.job.admin.controller;
+
+import com.xxl.job.admin.controller.annotation.PermissionLimit;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * index controller
+ *
+ * @author xuxueli 2015-12-19 16:13:16
+ */
+@Controller
+@RequestMapping("/wscm")
+public class WscmController {
+
+    @RequestMapping("/heart")
+    @PermissionLimit(limit=false)
+    @ResponseBody
+    public String heart() {
+        return "SUCCESS";
+    }
+
+}
