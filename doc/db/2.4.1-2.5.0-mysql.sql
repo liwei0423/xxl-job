f6f31c775798b75de8c9f0237770c40b9a175d81
fahai <786541343@qq.com>
2025-09-16 10:34:37 +0800

    添加2.4.1升级到2.5.0，mysql脚本

---
+++ doc/db/2.4.1-2.5.0-mysql.sql
@@ -0,0 +1,18 @@
+-- 删除原普通索引
+DROP INDEX CONCURRENTLY IF EXISTS i_g_k_v;
+
+-- 创建唯一索引
+CREATE UNIQUE INDEX CONCURRENTLY i_g_k_v
+    ON xxl_job_registry
+    USING btree (registry_group, registry_key, registry_value);
+
+CREATE INDEX i_jobid_jobgroup ON xxl_job_log (job_id,job_group);
+
+CREATE INDEX i_job_id ON xxl_job_log (job_id);
+
+DROP INDEX CONCURRENTLY IF EXISTS i_trigger_day;
+
+CREATE UNIQUE INDEX CONCURRENTLY i_trigger_day
+    ON xxl_job_log_report
+    USING btree (trigger_day);
+
